5dc2ad438d98e64cecb7a3c89a12ac65df551431
dgelessus <dgelessus@users.noreply.github.com>
2023-04-26 20:53:14 +0200

    Fix a weird indent

--- src/test/java/de/tlc4b/util/TLC4BTester.java
+++ src/test/java/de/tlc4b/util/TLC4BTester.java
@@ -4,11 +4,8 @@ package de.tlc4b.util;
 import de.tlc4b.TLC4B;
 
 public class TLC4BTester {
-	
-		public static void main(String[] args) throws Exception {
-			System.setProperty("apple.awt.UIElement", "true"); // avoiding pop up windows
-			TLC4B.test(args,true);
-		}
-
-
+	public static void main(String[] args) throws Exception {
+		System.setProperty("apple.awt.UIElement", "true"); // avoiding pop up windows
+		TLC4B.test(args,true);
+	}
 }
