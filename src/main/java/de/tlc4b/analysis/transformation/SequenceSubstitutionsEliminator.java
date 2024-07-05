bfe73a37ad793ce2a456afa8958ff59912dd5627
Jan Gruteser <jan.gruteser@hhu.de>
2024-07-05 17:43:20 +0200

    replace already assigned identifiers by primed identifiers in sequential substitutions

--- src/main/java/de/tlc4b/analysis/transformation/SequenceSubstitutionsEliminator.java
+++ src/main/java/de/tlc4b/analysis/transformation/SequenceSubstitutionsEliminator.java
@@ -3,8 +3,7 @@ package de.tlc4b.analysis.transformation;
 import de.be4.classicalb.core.parser.analysis.DepthFirstAdapter;
 import de.be4.classicalb.core.parser.node.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -14,16 +13,127 @@ import java.util.stream.Collectors;
  */
 public class SequenceSubstitutionsEliminator extends DepthFirstAdapter {
 
-	public static void eliminateSequenceSubstitutions(Start start) {
-		new SequenceSubstitutionsEliminator(start);
-	}
+	private ASequenceSubstitution topLevel = null;
+	private boolean initialisationMode = false, replacementMode = false;
+	private final Set<List<String>> assignments = new HashSet<>();
+	private final Set<Node> primeNodes = new HashSet<>();
 
-	private SequenceSubstitutionsEliminator(Start start) {
+	public SequenceSubstitutionsEliminator(Start start) {
 		start.apply(this);
 	}
 
 	@Override
-	public void caseASequenceSubstitution(ASequenceSubstitution node) {
+	public void inAAssignSubstitution(AAssignSubstitution node) {
+		if (topLevel == null)
+			return;
+
+		for (PExpression lhs : node.getLhsExpression()) {
+			if (lhs instanceof AIdentifierExpression) {
+				assignments.add(identifierToStringList((AIdentifierExpression) lhs));
+			}
+			// TODO: what to do with AFunctionExpression?
+		}
+	}
+
+	@Override
+	public void outAAssignSubstitution(AAssignSubstitution node) {
+		if (topLevel == null)
+			return;
+
+		replacementMode = true;
+		for (PExpression rhs : node.getRhsExpressions()) {
+			rhs.apply(this);
+		}
+		replacementMode = false;
+	}
+
+	@Override
+	public void inABecomesElementOfSubstitution(ABecomesElementOfSubstitution node) {
+		if (topLevel == null)
+			return;
+
+		for (PExpression lhs : node.getIdentifiers()) {
+			if (lhs instanceof AIdentifierExpression) {
+				assignments.add(identifierToStringList((AIdentifierExpression) lhs));
+			}
+		}
+	}
+
+	@Override
+	public void outABecomesElementOfSubstitution(ABecomesElementOfSubstitution node) {
+		if (topLevel == null)
+			return;
+
+		replacementMode = true;
+		node.getSet().apply(this);
+		replacementMode = false;
+	}
+
+	@Override
+	public void inABecomesSuchSubstitution(ABecomesSuchSubstitution node) {
+		if (topLevel == null)
+			return;
+
+		for (PExpression lhs : node.getIdentifiers()) {
+			if (lhs instanceof AIdentifierExpression) {
+				assignments.add(identifierToStringList((AIdentifierExpression) lhs));
+			}
+		}
+	}
+
+	@Override
+	public void outABecomesSuchSubstitution(ABecomesSuchSubstitution node) {
+		if (topLevel == null)
+			return;
+
+		replacementMode = true;
+		node.getPredicate().apply(this);
+		replacementMode = false;
+	}
+
+	@Override
+	public void caseAIdentifierExpression(AIdentifierExpression node) {
+		if (topLevel == null)
+			return;
+
+		System.out.println(node);
+		if (!initialisationMode && replacementMode && assignments.contains(identifierToStringList(node))) {
+			System.out.println("SUCC" + node);
+			primeNodes.add(node);
+		}
+	}
+
+	@Override
+	public void inAInitialisationMachineClause(AInitialisationMachineClause node) {
+		initialisationMode = true;
+	}
+
+	@Override
+	public void outAInitialisationMachineClause(AInitialisationMachineClause node) {
+		initialisationMode = false;
+	}
+
+	@Override
+	public void inASequenceSubstitution(ASequenceSubstitution node) {
+		if (topLevel == null) {
+			topLevel = node;
+		}
+	}
+
+	@Override
+	public void outASequenceSubstitution(ASequenceSubstitution node) {
+		if (topLevel.equals(node)) {
+			topLevel = null;
+			assignments.clear();
+		}
 		node.replaceBy(new AParallelSubstitution(new ArrayList<>(node.getSubstitutions())));
+	}
+
+	private static List<String> identifierToStringList(AIdentifierExpression identifier) {
+		return identifier.getIdentifier().stream().map(Token::getText).collect(Collectors.toList());
+	}
+
+	public Set<Node> getPrimeNodes() {
+		return primeNodes;
 	}
 }
