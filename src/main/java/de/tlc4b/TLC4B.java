c5448ff35d7ecec3a6b54a8f15f529bc56daef65
Jan Gruteser <jan.gruteser@hhu.de>
2024-07-10 09:56:52 +0200

    move cli options to separate class

--- src/main/java/de/tlc4b/TLC4B.java
+++ src/main/java/de/tlc4b/TLC4B.java
@@ -24,35 +24,11 @@ import de.tlc4b.tlc.TLCResults;
 import de.tlc4b.util.StopWatch;
 import org.apache.commons.cli.*;
 
+import static de.tlc4b.TLC4BCliOptions.*;
 import static de.tlc4b.util.StopWatch.Watches.*;
 import static de.tlc4b.MP.*;
 
 public class TLC4B {
-
-	public static final String NODEAD = "nodead";
-	public static final String NOTLC = "notlc";
-	public static final String NOTRANSLATION = "notranslation";
-	public static final String NOGOAL = "nogoal";
-	public static final String NOINV = "noinv";
-	public static final String NOASS = "noass";
-	public static final String WDCHECK = "wdcheck";
-	public static final String SYMMETRY = "symmetry";
-	public static final String TOOL = "tool";
-	public static final String TMP = "tmp";
-	public static final String NOLTL = "noltl";
-	public static final String LAZYCONSTANTS = "lazyconstants";
-	public static final String TESTSCRIPT = "testscript";
-	public static final String NOTRACE = "notrace";
-	public static final String DEL = "del";
-	public static final String PARINVEVAL = "parinveval";
-	public static final String LOG = "log";
-	public static final String MAXINT = "maxint";
-	public static final String DEFAULT_SETSIZE = "default_setsize";
-	public static final String MININT = "minint";
-	public static final String WORKERS = "workers";
-	public static final String CONSTANTSSETUP = "constantssetup";
-	public static final String LTLFORMULA = "ltlformula";
-	public static final String VERBOSE = "verbose";
 
 	private String filename;
 	private File mainfile;
@@ -227,8 +203,7 @@ public class TLC4B {
 
 		if (TLC4BGlobals.isRunTLC()) {
 			MP.TLCOutputStream.changeOutputStream();
-			TLCRunner.runTLC(tlc4b.machineFileNameWithoutFileExtension,
-					tlc4b.buildDir);
+			TLCRunner.runTLC(tlc4b.machineFileNameWithoutFileExtension, tlc4b.buildDir);
 			MP.TLCOutputStream.resetOutputStream();
 			TLCResults results = new TLCResults(tlc4b.tlcOutputInfo);
 			results.evalResults();
@@ -506,37 +481,6 @@ public class TLC4B {
 
 	public File getMainFile(){
 		return this.mainfile;
-	}
-
-	private static Options getCommandlineOptions() {
-		Options options = new Options();
-
-		options.addOption(NODEAD, "do not look for deadlocks (for model check, animate, execute)");
-		options.addOption(NOTLC, "");
-		options.addOption(NOTRANSLATION, "");
-		options.addOption(NOGOAL, "");
-		options.addOption(NOINV, "");
-		options.addOption(NOASS, "");
-		options.addOption(WDCHECK, "");
-		options.addOption(SYMMETRY, "");
-		options.addOption(TOOL, "");
-		options.addOption(TMP, "");
-		options.addOption(NOLTL, "");
-		options.addOption(LAZYCONSTANTS, "");
-		options.addOption(TESTSCRIPT, "");
-		options.addOption(NOTRACE, "");
-		options.addOption(DEL, "");
-		options.addOption(PARINVEVAL, "");
-		options.addOption(LOG, true, "write statistics to CSV file");
-		options.addOption(MAXINT, true, "");
-		options.addOption(DEFAULT_SETSIZE, true, "");
-		options.addOption(MININT, true, "");
-		options.addOption(WORKERS, true, "");
-		options.addOption(CONSTANTSSETUP, true, "use constants found by ProB for TLC model checking");
-		options.addOption(LTLFORMULA, true, "");
-		options.addOption(VERBOSE, "put TLC4B in verbose mode");
-
-		return options;
 	}
 	
 }
